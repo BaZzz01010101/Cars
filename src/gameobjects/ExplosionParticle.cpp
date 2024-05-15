b7439014c1c6361a6081270108f28b067f4ad31c
Andrey Borzylo <andy2002ua@gmail.com>
2024-05-15 13:43:13 +0200

    Inherit *ExplosionParticle* from *DynamicObject* to get rid of repeating code

--- src/gameobjects/ExplosionParticle.cpp
+++ src/gameobjects/ExplosionParticle.cpp
@@ -2,7 +2,8 @@
 #include "ExplosionParticle.h"
 #include "Helpers.h"
 
-namespace game{
+namespace game
+{
   ExplosionParticle ExplosionParticle::random(vec3 position, float minSize, float maxSize, float minSpeed, float maxSpeed, float minAngularSpeed, float maxAngularSpeed, float minLifeTime, float maxLifeTime, float gravity)
   {
     vec2 v0 = vec2::randomInRing(minSize, maxSize);
@@ -10,34 +11,28 @@ namespace game{
     vec2 v2 = -(v1 + v0).normalized() * randf(minSize, maxSize);
     quat rotation = quat::random();
 
-    return {
-      .lastPosition = position,
-      .lastRotation = rotation,
-      .position = position,
-      .velocity = vec3::randomInHollowSphere(minSpeed, maxSpeed),
-      .rotation = rotation,
-      .angularVelocity = vec3::randomInHollowSphere(minAngularSpeed, maxAngularSpeed),
-      .lifeTime = randf(minLifeTime, maxLifeTime),
-      .gravity = gravity,
-      .vertices = {
-        v0.intoXZWithY(0),
-        v1.intoXZWithY(0),
-        v2.intoXZWithY(0),
-      }
-    };
+    ExplosionParticle p;
+    p.position = position;
+    p.rotation = rotation;
+    p.lastPosition = position;
+    p.lastRotation = rotation;
+    p.velocity = vec3::randomInHollowSphere(minSpeed, maxSpeed);
+    p.angularVelocity = vec3::randomInHollowSphere(minAngularSpeed, maxAngularSpeed);
+    p.lifeTime = randf(minLifeTime, maxLifeTime);
+    p.gravity = gravity;
+    p.vertices[0] = v0.intoXZWithY(0);
+    p.vertices[1] = v1.intoXZWithY(0);
+    p.vertices[2] = v2.intoXZWithY(0);
+
+    return p;
   }
 
   void ExplosionParticle::update(float dt)
   {
-    lastPosition = position;
-    lastRotation = rotation;
+    DynamicObject::update(dt);
 
-    position += velocity * dt;
     velocity -= 0.5f * velocity * dt;
     velocity.y -= gravity * dt;
-    vec3 dr = angularVelocity * dt;
-    rotation = rotation * quat::fromEuler(dr.y, dr.z, dr.x);
-    rotation.normalize();
     lifeTime -= dt;
   }
 
--- src/gameobjects/ExplosionParticle.h
+++ src/gameobjects/ExplosionParticle.h
@@ -1,20 +1,14 @@
 #pragma once
 #include "Config.h"
+#include "DynamicObject.h"
 
 namespace game
 {
 
-  struct ExplosionParticle
+  struct ExplosionParticle : public DynamicObject
   {
     static ExplosionParticle random(vec3 position, float minSize, float maxSize, float minSpeed, float maxSpeed, float minAngularSpeed, float maxAngularSpeed, float minLifeTime, float maxLifeTime, float gravity);
 
-    vec3 lastPosition = vec3::zero;
-    quat lastRotation = quat::identity;
-
-    vec3 position = vec3::zero;
-    vec3 velocity = vec3::zero;
-    quat rotation = quat::identity;
-    vec3 angularVelocity = vec3::zero;
     float lifeTime = 0;
     float gravity = 9.8f;
     vec3 vertices[3] = { vec3::zero,  vec3::zero,  vec3::zero };

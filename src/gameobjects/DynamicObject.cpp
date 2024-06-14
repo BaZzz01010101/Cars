60267996ee4c7302beacff240f809ef62e6b8ede
Andrey Borzylo <andy2002ua@gmail.com>
2024-06-14 15:07:07 +0200

    Add TODO

--- src/gameobjects/DynamicObject.cpp
+++ src/gameobjects/DynamicObject.cpp
@@ -12,5 +12,17 @@ namespace game
     vec3 dr = angularVelocity * dt;
     rotation = rotation * quat::fromEuler(dr.y, dr.z, dr.x);
     rotation.normalize();
+
+    // TODO: consider replace with more performant solution
+    /*
+      quat omega = quat { dr.x, dr.y, dr.z, 0.0f };
+      quat q = rotation * omega;
+      rotation.x += 0.5f * q.x;
+      rotation.y += 0.5f * q.y;
+      rotation.z += 0.5f * q.z;
+      rotation.w += 0.5f * q.w;
+  
+      rotation.normalize();
+    */
   }
 }
